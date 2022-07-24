a71b4514b53bb851aa73871fba968b49b678c30a
Yandi Farinango <71475457+yandi-farinango@users.noreply.github.com>
2022-07-24 15:54:52 -0500

    Updating LongestConsecutiveSequence

--- LongestConsecutiveSequence/main.py
+++ LongestConsecutiveSequence/main.py
@@ -16,7 +16,24 @@ length = 9
 
 
 """
+We can use a set! 
 
+We create a set from nums array 
+
+And as were iterating through the array, 
+we check if n - 1 is in numSet
+
+if n-1 is NOT in numset, 
+we can say it is the START of a sequence
+
+Now, if we're at the start of a sequence, 
+we declare a counter, length, 
+
+length will be updated if, 
+n + length exists in our set
+
+we'll update maxLength 
+to return the longest sequence length
 """
 
 
@@ -34,11 +51,20 @@ class Solution(object):
             # n is the start of a sequence if
             # n-1 does NOT exist in nums
             if (n - 1) not in numSet:
+                length = 0
+                # checking if n + length exists in our set
+                # n + length is a reference to our next consecutive numbers
+                while (n + length) in numSet:
+                    length += 1
+                # Updating LONGEST
+                longest = max(length, longest)
 
-
-
-
-
+        return longest
 
 
 if __name__ == '__main__':
+    nums1 = [100, 4, 200, 1, 3, 2]
+    nums2 = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1]
+
+    print(Solution().longestConsecutive(nums1))
+    print(Solution().longestConsecutive(nums2))
