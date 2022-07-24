04fee01d0de6746ba77df0d5439f3d6951000c42
Yandi Farinango <71475457+yandi-farinango@users.noreply.github.com>
2022-07-24 13:30:31 -0500

    Updating Valid Sudoku

--- ValidSudoku/main.py
+++ ValidSudoku/main.py
@@ -7,55 +7,85 @@ according to the following:
     Each col must contain digits 1-9 w/o repetition
     Each 3x3 subBox must contain digits 1-9 w/o repetition
 """
+
+
+"""
+This problem is very similar to eariler array problems, 
+where we can add to a hashset
+to determine whether a variable already exists in the hashset 
+
+The tricky thing here is dealing w/ the subsquares 
+
+To solve this we will do the following:
+    Think of the subsquares as 3x3 grid 
+    X,Y vals from 1-3
+    To get this reference we will be using a SET
+
+    our SET will (r//3, c//3)
+
+    When we do a floor division, 
+    our result is rounded down
+    to the nearest whole number 
+
+    Ex: We have 9 rows (0 - 8)
+    Rows 0-2, when // by 3 
+    will return 0
+
+    Similar will happen for cols
+
+    And so, we now have a reference to 
+    our 3x3 subsquares 
+
+
+Now... in order to be able to use a set as a reference 
+We need to use collections.defaultdict(set)
+
+Once we've declared our defaultdict(set), 
+the logic is similar to easier problems 
+
+We traverse through board, 
+    -dont forget
+    if we come across "." 
+    we will simply continue
+
+check if board[r][c] exists within the respective dict
+If so we return FALSE
+If not, we add to the respective dict
+At the end return True
+
+"""
+
+
 import collections
 
 
 class Solution(object):
     def isValidSudoku(self, board):
-        # Creating hashsets for cols, rows, squares
-        # we use collections.defaultdict(set)
 
-        # The tricky thing here is why we use collections.defaultdict(set)
-        # We'll use defaultdict(set)
-        # b/c we'll be using a SET
-        # for our subSquare dictionary KEY
-
-        cols = collections.defaultdict(set)
-        rows = collections.defaultdict(set)
+        # default.dict(set)
+        row = collections.defaultdict(set)
+        col = collections.defaultdict(set)
         squares = collections.defaultdict(set)
 
+        # use in range(9)
+        # we were given 9x9 as board size
         for r in range(9):
+            # ignore "."
             for c in range(9):
-                # first check if position is empty
                 if board[r][c] == ".":
                     continue
-                # check if board[r][c] value exists in row[r]
-                # check if board[r][c] value exists in col[c]
-
-                # check if board[r][c] value exists
-                # in subSquare[  (r//3, c//3)  ]
-
-                # subSquare keys are sets!!
-                # This is why we used defaultdict(set) above
-                # Also, when we do //
-                # we are doing integer division
-                # where our answer will be rounded down
-                # to nearest whole number
-
-                if(board[r][c] in rows[r] or
-                   board[r][c] in cols[c] or
-                   board[r][c] in squares[(r // 3, c //3)]):
+                # checking if board[r][c] exists in either row, col, or board
+                if (board[r][c] in row[r] or
+                        board[r][c] in col[c] or
+                        board[r][c] in squares[(r // 3, c // 3)]):
                     return False
+                # adding to our dict(set)
+                # if board[r][c] does not already exist
+                row[r].add(board[r][c])
+                col[c].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
 
-                # Similar to like-problems
-                # if we do not return False above,
-                # we append, or in this case -add,
-                # to our dictionary
-                cols[c].add(board[r][c])
-                rows[r].add(board[r][c])
-                squares[(r//3, c//3)].add(board[r][c])
         return True
-
 
 if __name__ == '__main__':
     board1 = [["5", "3", ".", ".", "7", ".", ".", ".", "."],
