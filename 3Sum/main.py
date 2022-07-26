a658b4898e13c07e3b2e6fcdc866a639db2b932e
Yandi Farinango <71475457+yandi-farinango@users.noreply.github.com>
2022-07-26 14:05:15 -0500

    Updated 3Sum

--- 3Sum/main.py
+++ 3Sum/main.py
@@ -19,7 +19,36 @@ contain duplicate triplets
 """
 
 """
-If we sort nums,
+Given nums, 
+we are to return all triplets 
+adding to 0
+
+    Ex: nums[i] + nums[j] + nums[k] = 0
+    return [  [nums[i], nums[j], nums[k]]  ]
+    
+Our solution must not contain duplicate triplet sets 
+
+B/c NUMS may contain duplicates,
+if we were to iterate through nums
+using loops, 
+AND, 
+we came across a duplicate
+in position nums[i], 
+we would be returning duplicate triplet sets
+
+SO, 
+we know that nums[i] MUST be unique 
+
+
+if we first SORT our array, 
+before iterating through, 
+we can skip repeated nums[i] values
+
+SORTING also allows us to use twoSum2 
+solution 
+for finding n[j] and n[k]! 
+
+
 
 """
 
@@ -27,26 +56,58 @@ If we sort nums,
 class Solution(object):
     def threeSum(self, nums):
 
+        # result will be returned as [[]]
         res = []
+
+        # we first want to sort nums array
         nums.sort()
 
-        for i, a in enumerate(nums):
-            if i > 0 and a == nums[i - 1]:
+        for index, value in enumerate(nums):
+            # we don't want to re-use for n[i] position
+
+            # if were NOT at first index
+            # and our value is == to the previous
+            # we ignore
+            if index > 0 and value == nums[index - 1]:
                 continue
 
-            left, right = i + 1, len(nums) -1
+            # Now we can implement our twoPointer2 sol
+            # we set our pointers on remaining portion
+            # of our nums array
+            # if we're at idx
+            # our remaining portion begins at idx + 1
+            # and ends at len(nums) - 1
+
+            left, right = index + 1, len(nums) - 1
 
             while left < right:
-                threeSum = a + nums[left] + nums[right]
+                # we don't do nums[idx] + nums[left] +...
+                # b/c we are idx, val ENUMERATING in our loop
+                # we can just call our val
+                threeSum = value + nums[left] + nums[right]
 
                 if threeSum > 0:
                     right -= 1
                 elif threeSum < 0:
                     left += 1
                 else:
-                    res.append([a, nums[left], nums[right]])
+                    # we've found a triplet set!
+                    # append to result
+                    res.append([value, nums[left], nums[right]])
 
+                    # we want to shift our left pointer
+                    # to iterate through the rest of nums
+                    # and see if we potentially have other triplet groups
                     left += 1
+                    # we want to find NEW triplet groups
+                    # so we ignore duplicates
+
+                    # if nums[left] is the same
+                    # as nums[left] before,
+                    # it is a duplicate
+
+                    # we shift our left pointer to ignore duplicates
+                    # but we also ALWAYS want our left pointer to be less than right
                     while nums[left] == nums[left - 1] and left < right:
                         left += 1
         return res
