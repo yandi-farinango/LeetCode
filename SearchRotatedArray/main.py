14077366f678d98a571ff0dcf07d64118e4b09cd
Yandi Farinango <71475457+yandi-farinango@users.noreply.github.com>
2022-09-20 08:00:12 -0500

    Updated SearchRotatedArray

--- SearchRotatedArray/main.py
+++ SearchRotatedArray/main.py
@@ -15,63 +15,56 @@ if it is in nums
     [0, 1, 2, 4, 5, 6, 7]
 
     rotated at pivot index 3
-    [7, 6, 5, 4, 0, 1, 2]
+    [4, 5, 6, 7, 0, 1, 2]
 
 
 """
 
 """
-We can see that after rotation, 
-our array can be seperated into two sections 
-LEFT section contains LARGER numbers in DESCENDING order
-RIGHT section contains SMALLER numbers in ASCENDING order
+Nums is:
+    sorted in ascending order 
+    then, possibly rotated at an unknown index 
+    
+    ex: [0,1,2,4,5,6,7] -> [4,5,6,7,0,1,2]
+    rotated at pivot index 3
+    
+We can see nums can be divided into two sections 
+Left section -> larger numbers 
+Right section -> smaller numbers 
 
-If we try binary search 
-
-We'll start with our pointers initialized at
-left = 0 
+We use two pointers 
+set at left = 0 
 right = len(nums) - 1
 
-We can get some information about our MIDPOINT 
+Our first step would be to determine which section we need to search
 
-MIDPOINT <= LEFT
-MIDPOINT corresponds to our LEFT section
-i.e fits with numbers in DESCENDING order from LEFT -> RIGHT 
+if nums[left] <= nums[mid]
 
+We know that our MID corresponds to our LEFT portion 
 
-Once we've identified how our nums array is structured
+    if we are in the LEFT portion 
+    
+    and our target > nums[mid] or target < nums[left]
+    we have to search RIGHT 
+    
+    else search LEFT 
+    
+if we know that our MID corresponds to our RIGHT portion
 
-Our next step would be 
-which PORTION we want to search 
-
-
-IF our MID corresponds to our LEFT portion
-        if TARGET > nums[mid], TARGET might lie in our RIGHT portion 
-        also if TARGET < nums[left], TARGET might lie in our RIGHT PORTION
-        
-        we shift left pointer UP 
-        left = mid + 1
-        
-        
-        else:
-        right = mid - 1 
-
-
-IF our MID corresponds to our RIGHT,
-        if TARGET < nums[mid], TARGET might lie in our LEFT portion 
-        also if TARGET > nums[left], TARGET might lie in our LEFT portion 
-        
-        we shift our right pointer DOWN 
-        right = mid - 1 
-        
-        else 
-        left = mid - 1
-        
+    if we are in the RIGHT portion 
+    
+    and our target < nums[mid] or target > nums[right]
+    we have to search LEFT 
+    
+    else search RIGHT
+    
+return -1 
 """
 
 
 class Solution(object):
     def search(self, nums, target):
+
         left, right = 0, len(nums) - 1
 
         while left <= right:
@@ -80,31 +73,34 @@ class Solution(object):
             if target == nums[mid]:
                 return mid
 
-            # mid belongs to LEFT portion
-            # ie LARGER numbers DESCENDING order
+            # we're in the LEFT portion
             if nums[left] <= nums[mid]:
-
-                # if our MID belongs to our left
-                # and if TARGET > nums[mid] TARGET might lie in our RIGHT portion
-                # also if TARGET < nums[left] TARGET might lie in our RIGHT portion
+                # search RIGHT
                 if target > nums[mid] or target < nums[left]:
                     left = mid + 1
+                # search LEFT
                 else:
-                    right = mid - 1
+                    right = mid -1
 
-
-            # mid belongs to RIGHT portion
-            # ie SMALLER numbers ASCENDING order
+            # we're in the RIGHT portion
             else:
-                # if MID belongs to our RIGHT
-                # and if TARGET > nums[mid]
-                # also if target > nums[left]
-                if target < nums[mid] or target > nums[left]:
+                # search LEFT
+                if target < nums[mid] or target > nums[right]:
                     right = mid - 1
+                # search RIGHT
                 else:
                     left = mid + 1
 
         return -1
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
