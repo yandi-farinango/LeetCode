99f212200729520fac0b8da7d07718675058c14c
Yandi Farinango <71475457+yandi-farinango@users.noreply.github.com>
2022-09-26 11:49:21 -0500

    Solution 2 did not work Removed

--- RemoveNthNodeFromEndOfList/main.py
+++ RemoveNthNodeFromEndOfList/main.py
@@ -42,41 +42,14 @@ class Solution(object):
 
         return dummy.next
 
-
-class Solution2(object):
-    def removeNthFromEnd(self, head, n):
-        slow, fast = head, head
-
-        # get fast pointer in correct position
-        i = 0
-        while i < n:
-            fast = fast.next
-            i += 1
-
-        # shift pointers
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next
-
-        # remove nth node
-        slow.next = slow.next.next
-
-        return head
-
-
 if __name__ == '__main__':
     ll = LinkedList()
     ll.head = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
     n = 2
 
+    printLinkedList(ll.head)
+
+    print('--Removed--')
     ans = Solution().removeNthFromEnd(ll.head, n)
     printLinkedList(ans)
 
-    # Solution 2
-    ll2 = LinkedList()
-    ll2.head = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
-    n = 2
-
-    ans2 = Solution2().removeNthFromEnd(ll2.head, n)
-    print("--Solution 2--")
-    printLinkedList(ans2)
