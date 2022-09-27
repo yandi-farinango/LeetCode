315b6c5630c33c7456431fef36cfa7eb53dcc88b
Yandi Farinango <71475457+yandi-farinango@users.noreply.github.com>
2022-09-27 11:54:00 -0500

    Updated solution

--- TimeBasedKeyValueStore/main.py
+++ TimeBasedKeyValueStore/main.py
@@ -14,87 +14,44 @@ at a certain timestamp
 
 class TimeMap(object):
     def __init__(self):
-        self.store = {} # key : [[value, timestamp]]
+        self.ans = {}
 
     def set(self, key, value, timestamp):
-        """
-        Stores key with value at a given timestamp
+        # if key is not in our dictionary,
+        # create empty list where we will be appending [value, timestamp]
+        if key not in self.ans:
+            self.ans[key] = []
 
-        Ex: set("foo", "bar", 1)
-            Key     | Value <val, time>
-            "foo"   | [["bar", 1]]
+        self.ans[key].append([value, timestamp])
 
-            set("foo", "bar2", 2)
-            Key     | Value <val, time>
-            "foo"   | [["bar", 1], ["bar2", 2]]
-
-        :param key: str
-        :param value: str
-        :param timestamp: int
-        :return: None
-        """
-
-        # if key does not exist in self.store
-        # we create key and map it to an empty list
-        # ex: key:[]
-        if key not in self.store:
-            self.store[key] = []
-
-        # append [value, timestamp]
-        # ex: key: [[value, timestamp]]
-        self.store[key].append([value, timestamp])
 
     def get(self, key, timestamp):
-        """
-        Returns a value such that set was called previously with
-        timestamp_prev <= timestamp
+        ans = ""
 
-        If there are multiple such values, returns the value associated with the largest timestamp_prev
-        If there are no value, returns ""
+        # getting list of values associated with key
+        values = self.ans.get(key, [])
 
-        Ex: Key     | Value <val, time>
-            "foo"   | [["bar", 1], ["bar2", 2], ["bar6", 6]]
+        # now we want to search through values
+        # and return the value with the closest matching timestamp to timestamp
 
-            get("foo", 2)
-            return "bar2"
-
-            get("foo", 4)
-            return "bar2
-                * bar2 is the closest val <= timestamp 4
-                return bar2
-
-            We can find the closest val <= timestamp using BINARY SEARCH!!!
-
-        :param key: str
-        :param timestamp: int
-        :return: str
-        """
-
-        # if key doesnt exist,  i.e there are no values
-        # return ""
-        res = ""
-
-        # return list of values
-        # if we don't find a match, return empty list []
-        values = self.store.get(key, [])
+        # initialize pointers
+        left, right = 0, len(values) - 1
 
         # Binary Search
-        left, right = 0, len(values)-1
-
         while left <= right:
             mid = (left + right)//2
 
-            # we are looking for the closest val <= timestamp
             if values[mid][1] <= timestamp:
-                # update res
-                res = values[mid][0]
+                # update ans
+                ans = values[mid][0]
 
                 # keep searching, shift left UP
                 left = mid + 1
             else:
+                # search down
                 right = mid - 1
-        return res
 
+        return ans
 
 
 if __name__ == '__main__':
