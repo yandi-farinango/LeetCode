f87a388fc1db0168f6631040925caf38e474432e
Yandi Farinango <71475457+yandi-farinango@users.noreply.github.com>
2022-10-03 08:43:00 -0500

    Updated Solution

--- AddTwoNumbers/main.py
+++ AddTwoNumbers/main.py
@@ -28,61 +28,65 @@ following the same format
 """
 
 """
-We can actually solve this problem
-taking an elementary school approach 
+We can actually sum this pretty straightforward! 
 
-    Ex: 123
-      + 456
-      ------
-      
-In solving the expression above, 
-we're going to start adding at the ones place 
+If we remember from elementary addition 
+             1
+    Ex:     125
+          + 456
+          ------
+            581
+            
+We actually start adding from the ones place 
+we carry the one - from the tens place 
+etc. 
 
-the lists are already given to us starting at the ones! 
+Bc our numbers are given in REVERSE order 
+we can start adding vals at the head of the linked lists! 
 
-the thing we'll have to take note of is what happens when 
-we add values > 10 
-and we have 
+We'll create a dummy node where well be appending/connecting 
+our ans to 
 
-we can get the carried tens-place value by //10
-and we can get the ones-place value by % 10 
+We'll need a carry variable to hold tens-place vals 
+in the event that we need to carry 
 
-We'll use initialize a dummy node 
-and we'll append our vals to dummy.next 
+Well get the carry val by val//10 
+well get the val by val % 10 
 
-return dummy.next 
 """
 
 
 class Solution(object):
-    def addTwoNumbers(self, list1, list2):
-        # intialize dummy
+    def addTwoNumbers(self, l1, l2):
+        # initialize dummy node
         dummy = ListNode()
 
-        # pointer
+        # pointer to dummy
         current = dummy
 
-        # initialize carry at 0
+        # intialize carry at 0
         carry = 0
 
-        while list1 or list2 or carry:
-            v1 = list1.val if list1 else 0
-            v2 = list2.val if list2 else 0
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
 
-            # add
-            val = v1 + v2 + carry
+            # add vals
+            total_sum = v1 + v2 + carry
 
-            # get carried and val
-            carry = val // 10
-            val = val % 10
+            # get val to carry
+            carry = total_sum // 10
 
-            # append
+            # get ones - place val
+            val = total_sum % 10
+
+            # connect pointer
             current.next = ListNode(val)
 
             # shift pointers
             current = current.next
-            list1 = list1.next if list1 else None
-            list2 = list2.next if list2 else None
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
 
         return dummy.next
 
