c3089773a479a966bee2dca66582c290b0f48458
Yandi Farinango <71475457+yandi-farinango@users.noreply.github.com>
2022-10-13 10:56:33 -0500

    Updated Comments

--- ConstructBinaryTreePreorderInorderTraversal/main.py
+++ ConstructBinaryTreePreorderInorderTraversal/main.py
@@ -19,51 +19,59 @@ construct and return the binary tree
 """
 
 """
-We need to know:
-- First number in preorder traversal is always going to be the ROOT
-- We can determine which subtree other values correspond to by looking at the inorder traversal 
-- We can look for the ROOT val in our inorder traversal array 
-- If we can identify the ROOT from our preorder traversal,
-  we can see that, in our inorder traversal, numbers to the left of ROOT
-  will correspond to the left subtree, same for right 
-  
+We can see from the given arrays 
+That we can solve this problem recursively 
 
-For our code, 
-we are getting the first value in our preorder array, 
-looking up that value in our inorder array 
+basecase 
+if not preorder or not inorder return None
 
-from our inorder array, we can get a sense of what values are in the left, right subtrees
 
-We know where to partition our preorder array; ie partition our tree; because 
-the len of the values to the left 
-and len of the values to the right 
+The root node is always found at preorder[0]
 
-We can do this recursively! 
 
-# Creating the recursive function 
-Base case:  if we don't have any nodes to traverse ie 
-            if not preorder or not inorder
-            :return null
-            
-we'll create our root node      * root is at position preorder[0]
-root = TreeNode(preorder[0])
+To create the subtrees, we need to know where to partition 
+the inorder array 
 
-We want to find the position of our "root" in our inorder array
+We'll need to get the index of the root within our inorder array
 mid = inorder.index(preorder[0])
 
 
-We can build our tree recursively
-where we can pass in our partitioned preorder and inorder sub-arrays 
+To build the tree recursively, 
+we pass in 
 
-For left subtree, root.left, we want 
-preorder - every node to the left of mid; we pass in: preorder[1: mid+1]
-inorder - every node up until mid; we pass in inorder[:mid]
 
-For right subtree, root.right, we want 
-preorder - every node to the right of mid; we pass in: preorder[mid+1:]
-inorder - every node to the right of mid; we pass in: inorder[mid + 1:]
+root.left = self.buildTree()
+      
+    For passing in preorder, 
+    We want the next root node; ie root.left 
+    to be found at the 0th index of preorder 
+    and we want pass in all the nodes that will be in the left subtree 
+    this partition is found at 
+    preorder[1: mid + 1]
 
-return root 
+
+    for passing in inorder, 
+    we want all the values that correspond to the left subtree 
+    ie, the values up to mid 
+    this partition is found at 
+    inorder[:mid]
+    
+
+root.right = self.buildTree()
+
+    for passing in preorder, 
+    we want the next root node; ie root.right 
+    to be found at the 0th index of preorder 
+    and we want to pass in all the nodes that will be in the right subtree
+    this partition is found at 
+    preorder[mid+1:]
+    
+    
+    for passing in inorder
+    we want all the values that correspond to the right subtree
+    ie the values after mid 
+    this partition is found at 
+    inorder[mid+1:]
 """
 
 
@@ -73,14 +81,13 @@ class Solution(object):
         if not preorder or not inorder:
             return None
 
-        # create root TreeNode; root found at position preorder[0]
+            # construct root
         root = TreeNode(preorder[0])
 
-        # find root position in inorder array
+        # get index
         mid = inorder.index(preorder[0])
 
-        # we can build our subtrees recursively
-        # we pass in our new preorder and inorder sub-arrays
+        # recursive calls for constructing subtrees
         root.left = self.buildTree(preorder[1:mid + 1], inorder[:mid])
         root.right = self.buildTree(preorder[mid + 1:], inorder[mid + 1:])
 
@@ -88,8 +95,8 @@ class Solution(object):
 
 
 if __name__ == '__main__':
-    preorder = [3, 9, 20, 15, 7]
-    inorder = [9, 3, 15, 20, 7]
+    preorder = [1, 2, 4, 5, 3, 6, 7]
+    inorder = [4, 2, 5, 1, 6, 3, 7]
 
     ans = Solution().buildTree(preorder, inorder)
 
