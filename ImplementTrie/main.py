e6088631448c377ca3f3ca02298fcb0d1adf6f7a
Yandi Farinango <71475457+yandi-farinango@users.noreply.github.com>
2022-10-16 12:07:52 -0500

    Updated Solution, updated comments

--- ImplementTrie/main.py
+++ ImplementTrie/main.py
@@ -10,15 +10,67 @@ Implement the trie class
 """
 
 """
-Implement TrieNode 
+To implement a Trie 
+we're going to create a tree-like structure 
+instead of having a next pointer, 
+each node will have a dictionary 
+mapping letter : node 
+
+In this way, we can store letters
+one after the other and have nodes as placeholders 
+
+    Ex:     root 
+            / 
+        {a: node}
+              /
+           {p: node}
+                /
+             {p: node}
+                 /
+               {l: node}
+                  /
+                {e: node}
+
+
+We insert values in this order 
+
+This allows us to perform search 
+and starts with operations 
+efficiently 
+
+When we insert new words, 
+words will be inserted according to their pre-fix 
+
+We will insert:
+
+        for char in word:
+            if char not in current.children:
+                current.children[char] = TrieNode()
+
+            # shift current pointer 
+            current = current.children[char]
+
+In this way, 
+if a word has a similar prefix 
+it will be connected to the longer word 
+
+We also want to have each node contain a 
+'endOfWord' boolean 
+the end of word boolean 
+lets us know when we've finished with a word 
+this allows for searching for words within our trie 
+
 """
+
+"""
+Implement TrieNode
+"""
+
 
 class TrieNode:
     def __init__(self):
         self.children = {}
         self.endOfWord = False
-
-        # children['a'] = TrieNode()
 
 
 class Trie(object):
@@ -26,72 +78,82 @@ class Trie(object):
         """
         initializes the trie object
 
-        initialize trie root
+        To initialize a trie object,
+        we'll want start by initializing the trie root
         """
+
         self.root = TrieNode()
 
     def insert(self, word):
         """
-        inserts word,
-        into trie
+        inserts word in the trie
 
-        We have to iterate through every character in the word
-        and structure trie tree
+        to insert words into the trie
+        we'll need to traverse chars in word
+        and create new trie nodes representing each char
         """
 
+        # pointer to root
         current = self.root
 
         for char in word:
-            # if the character is not in the trie's children,
-            # it hasnt been inserted yet
             if char not in current.children:
-                # This is how we insert new trie nodes
+                # map char: TrieNode
                 current.children[char] = TrieNode()
 
-            # move down the tries tree structure
+            # shift pointer
             current = current.children[char]
 
-        # we mark the last char as endOfWord
+        # mark endOfWord
         current.endOfWord = True
 
     def search(self, word):
         """
-        if word is in trie:
-            return True
+        checks trie and searches
+        if word in trie, return True
 
-        return False
+        In order to check if a word is in our trie,
+        we'll need to
+        - get a pointer to the root
+        - traverse each letter
+        - if char not in current.children; return False
+            * if all chars in our trie, we'll never execute return False
+
+        But, we also check if endOfWord
+        return current.endOfWord
+        ie previously marked as True if endOfWord
         """
 
+        # pointer to root
         current = self.root
 
         for char in word:
             if char not in current.children:
                 return False
 
-            # update current pointer
+            # shift pointer
             current = current.children[char]
 
-        # current should be the last character of the word
+        # check endOfWord
         return current.endOfWord
 
     def startsWith(self, prefix):
         """
-        if there is a previously inserted word
-            that starts with the given prefix
-            return True
+        Checks trie, if there is a word
+        that starts with the given prefix, return True
 
-        return False
-
-        Should be the same as search
-        except return current.endOfWord
+        This is similar to the search
+        except that we do not need to check for endOfWord
         """
 
+        # pointer to root
         current = self.root
 
         for char in prefix:
             if char not in current.children:
                 return False
 
+            # shift pointer
             current = current.children[char]
 
         return True
@@ -105,10 +167,9 @@ if __name__ == '__main__':
     print(trie.search('apple'))
     # returns False
     print(trie.search('app'))
+    # returns True
+    print(trie.startsWith('app'))
 
     trie.insert('app')
-
-    # return True
+    # returns True
     print(trie.search('app'))
-    # return True
-    print(trie.startsWith('ap'))
