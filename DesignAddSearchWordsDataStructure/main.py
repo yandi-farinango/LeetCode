59f88765f536c997130a9cc7d6cdf1ea7f54d092
Yandi Farinango <71475457+yandi-farinango@users.noreply.github.com>
2022-10-28 12:02:50 -0500

    Updated solution

--- DesignAddSearchWordsDataStructure/main.py
+++ DesignAddSearchWordsDataStructure/main.py
@@ -23,36 +23,30 @@ for search function
 """
 
 """
-Implement TrieNode
+Trie Node 
 """
-
-
 class TrieNode:
     def __init__(self):
         self.children = {}
         self.endOfWord = False
 
 
+"""
+Word Search
+"""
+
 class WordDictionary(object):
     def __init__(self):
         """
-        initializes WordDictionary object
-        to initialize WordDictionary as a trie prefix tree
-        we'll need to initialize a root node
+        initialize root
         """
 
         self.root = TrieNode()
 
     def addWord(self, word):
         """
-        adds a word to the dictionary
+        Adds words to the Trie
 
-        for adding words to a dictionary,
-        we'll go through each char
-        if char not in current.dictionary,
-        we can add it by mapping char: TrieNode()
-
-        we'll also have to set endOfWord = True
         """
 
         # pointer
@@ -60,62 +54,36 @@ class WordDictionary(object):
 
         for char in word:
             if char not in current.children:
-                # map char: TrieNode
                 current.children[char] = TrieNode()
 
             # shift pointer
             current = current.children[char]
 
-        # mark endOfWord
+        # end of word
         current.endOfWord = True
+
 
     def search(self, word):
         """
-        When searching for a word,
-        we want to return true if a word exists in our trie
-
-        we'll traverse the word
-        for i in range(len(word))
-        c = word[i]
-
-        if
-            char = '.', it can count towards any char,
-            this will have to be done recursively
-
-
-        else
-            if c not in current.children
-                return false
-
-            else
-                shift current pointer
-
-        return end of word
+        Searches for a certain word in the Trie
 
         """
 
         def dfs(root, j):
+            # pointer
             current = root
 
             for i in range(j, len(word)):
                 c = word[i]
 
+                # recursive section
                 if c == '.':
-                    # '.' can count towards any char,
-                    # we want to check all currents children braches
                     for child in current.children.values():
-
-                        # recursive call, skipping over .
-                        # by incrementing i
                         if dfs(child, i + 1):
-                            # if word search ends in .
-                            # we'll never execute below code,
-                            # so we want to return True
                             return True
 
                     return False
 
-                # non recursive
                 else:
                     if c not in current.children:
                         return False
@@ -124,10 +92,8 @@ class WordDictionary(object):
                     else:
                         current = current.children[c]
 
-            # return endOfWord
             return current.endOfWord
 
-        # return recursive call
         return dfs(self.root, 0)
 
 
