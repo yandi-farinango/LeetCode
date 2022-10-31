c03b5aca83ea1ec797d6320c0738e895e196d98b
Yandi Farinango <71475457+yandi-farinango@users.noreply.github.com>
2022-10-30 19:27:45 -0500

    Updated Solution and comments

--- TaskScheduler/main.py
+++ TaskScheduler/main.py
@@ -1,24 +1,31 @@
 """
-Given a characters array, Tasks,
-representing tasks a CPU needs to perform
+Given a chars array, Tasks,
+representing tasks
+a CPU needs to perform
 
-each letter corresponds to a different task
+where each letter is a different task
 
-Tasks may be done in any order
+tasks can done in any order
 
-Each task is done in 1 unit of time
-
-For each unit of time,
-the CPU can complete either 1 task or be idle
+Each task takes one unit of time to execute
 
 
-There is a non-negative int, n,
-represents the cool down, ie idle period,
-between two SAME tasks
+During a unit of time,
+the computer can either complete one task
+or be on idle
 
-Return the least number of times
-the CPU will take to finish
-all given tasks
+
+There is also a non-negative int, n,
+that represents the cool down period
+between two same tasks (ie same letter in Tasks array)
+
+ie the computer must wait
+n units of time
+before performing any two same tasks
+
+Return the least number of units of time
+that the CPU will take
+to finish all the given tasks
 
 
     Ex:
@@ -39,32 +46,60 @@ from collections import Counter
 To solve this
 
 We'll want get charCounts 
-we'll can do this using counter function 
+we'll do this using counter function 
 
-We'll want to start processing the most frequent task first 
-in order to do this, 
+We'll want to continuously the most frequent task first 
+ie after we've processed a task, we reduce the count 
+
+in order to do continuously process the most frequent, 
 we'll be using a maxHeap 
 
-We can add charCounts to our maxHeap 
-and pop the most frequent 
+we'll use a time counter,
+initialized time = 0,
+to keep track of time 
 
-When we pop the most frequent, 
-we'll want to append char to a que, 
-in order to keep track of idle times
+We'll pop from our maxHeap
+decrement the charCount and 
+increment our time counter 
 
-We'll be appending [-count, idleTime] to our que 
+we'll also be keeping track 
+of when we'll be able to process 
+another task of the same type ie same char 
+
+we can determine when a task is available
+to process again 
+by adding idle time at time task was processed 
+
+we'll use a que 
+to append(updatedCount after processing, time task can be processed again)
+
+when we reach a time 
+where tasks in our que 
+are available to be processed again, 
+we'll pop from our que 
+and add them back to our maxHeap 
+
+when a tasks charCount reaches 0 
+we no longer have to add it to our que 
+
+return time 
 
 
+* bc python doesnt have a maxHeap 
+we'll actually use a minHeap 
+but we'll make all counts negative 
 """
 
 
 class Solution(object):
     def leastInterval(self, tasks, n):
+
         # get charCount
         charCount = Counter(tasks)
 
-        # initialize maxHeap
+        # initialize maxHeap with negative counts
         maxHeap = [-count for count in charCount.values()]
+
         heapq.heapify(maxHeap)
 
         # initialize time counter
@@ -73,20 +108,27 @@ class Solution(object):
         # initialize que
         que = collections.deque()
 
-        # processing
         while maxHeap or que:
-            # increment timer
+            # increment time counter at each iteration
             time += 1
 
+            # pop most frequent
+            # and decrement cnt
             if maxHeap:
                 cnt = 1 + heapq.heappop(maxHeap)
-                if cnt != 0:
+
+                # append [cnt, time + idle] to que
+                if cnt:
                     que.append([cnt, time + n])
 
+            # when we reach a time
+            # such that a task can be processed again,
+            # we pop from our que and add it back to maxHeap
             if que and que[0][1] == time:
-                heapq.heappush(maxHeap, que.popleft()[0])
+                heapq.heappush(maxHeap, que.popleft()[0])   # we only need to add cnt
 
         return time
+
 
 
 if __name__ == '__main__':
