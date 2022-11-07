aad999e0d08da9e1b13ccb357aed07954afdf10d
Yandi Farinango <71475457+yandi-farinango@users.noreply.github.com>
2022-11-07 12:38:12 -0600

    Updated Solution

--- LinkedListCycle/main.py
+++ LinkedListCycle/main.py
@@ -1,55 +1,70 @@
-# This is a sample Python script.
+from LinkedList import *
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+"""
+Given the HEAD of a linked list,
 
+Determine if the linkedList has a cycle
 
-# def print_hi(name):
-#     # Use a breakpoint in the code line below to debug your script.
-#     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-#
-#
-# # Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
-#     print_hi('PyCharm')
-#
+There is a cycle if,
+some node in the list
+can be reached repeatedly
+by following the next pointer
+"""
 
+"""
+To solve, 
+we can use two pointers, 
+fast, slow
 
+We'll shift our fast pointer 
+fast = fast.next.next 
 
-class ListNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+and slow pointer 
+slow = slow.next 
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
+Since we'll be shifting our fast pointer 
+fast = fast.next.next 
+
+we can loop 
+while fast and fast.next
+
+If the two pointers ever meet 
+ie fast == slow 
+we know we have a cycle 
+return TRUE 
+"""
+
 
 class Solution(object):
     def hasCycle(self, head):
+        # initialize pointers
         fast, slow = head, head
 
-        while head.next:
-            slow = head.next
-            fast = head.next.next
+        # loop
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
 
-            if slow.val != fast.val:
-                return False
-            else:
+            if fast == slow:
                 return True
 
-ll = LinkedList()
+        return False
 
-ll.head = ListNode(3)
-l2 = ListNode(2)
-l3 = ListNode(0)
-l4 = ListNode(-4)
+if __name__ == '__main__':
+    ll = LinkedList()
 
-ll.head.next = l2
-l2.next = l3
-l3.next = l4
-l4.next = l2
+    ll.head = ListNode(3)
+    l2 = ListNode(2)
+    l3 = ListNode(0)
+    l4 = ListNode(-4)
 
-print(Solution.hasCycle(ll.head))
+    ll.head.next = l2
+    l2.next = l3
+    l3.next = l4
+    l4.next = l2
+
+    print(Solution().hasCycle(ll.head))
+
+
 
 
